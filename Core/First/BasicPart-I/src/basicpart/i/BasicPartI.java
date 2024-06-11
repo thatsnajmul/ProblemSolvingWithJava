eb944b09b77f553bbcb402d6466e6703aed04a43
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-11 12:30:11 +0600

    Add

--- Core/First/BasicPart-I/src/basicpart/i/BasicPartI.java
+++ Core/First/BasicPart-I/src/basicpart/i/BasicPartI.java
@@ -3,7 +3,25 @@ package basicpart.i;
 
 
 public class BasicPartI {
-
+    
+    //1--Print hello and your name on a separate lines
+    //2--Print the sum of two numbers
+    //3--Divide two numbers and print on the screen
+    //4--Print the result of the specified operations
+    //5--Display the product of two numbers
+    //6--Print the sum, multiply, subtract, divide and remainder of two numbers
+    //7--Print multiplication table of a number upto 10
+    //8--Display a specified pattern
+    //9--Display and compute specified expressions
+    //10--Compute a specified formula
+    //11--Find the area and perimeter of a circle
+    //12--Calculate and print the average of five numbers
+    //13--Print the area and perimeter of a rectangle
+    //14--Print an American flag on the screen
+    //15--Swap two variables
+    
+    
+    
 
     public static void main(String[] args) {
         

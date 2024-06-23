1d73db84241338a0271136d950a450ec4f738c15
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 15:58:24 +0600

    Add

--- Core/First/BasicPart-I/src/basicpartii/BasicPartII.java
+++ Core/First/BasicPart-I/src/basicpartii/BasicPartII.java
@@ -1,13 +1,107 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package basicpartii;
 
-/**
- *
- * @author Admin
- */
+
 public class BasicPartII {
+    
+    //151-- Find the value of specified expression
+    //152-- Accepts four integer from the user and prints equal
+    //153-- Accepts two double variables and test the specified condition
+    //154-- Print the contents of a two-dimensional Boolean array in specific format
+    //155-- Print an array after changing the rows and columns of a specified two-dimensional array
+    //156-- Returns the largest integer but not larger than the base-2 logarithm of a specified integer
+    //157-- Prove that Euclid’s algorithm computes the greatest common divisor of two positive given integers
+    //158-- Create a 2-D array such that A[i][j] is false if I and j are prime, otherwise true
+    //159-- Find the k largest elements in a specified array
+    //160-- Find the k smallest elements in a specified array
+    //161-- Find the kth smallest and largest element in a specified array
+    //162-- Find the numbers greater than the average of the numbers of a specified array
+    //163-- Accept an integer and convert it into a binary representation
+    //164-- Divide the two specified integers using subtraction operator
+    //165-- Move every positive number to the right and every negative number to the left of a given array of integers
+    //166-- Transform a given integer to String format
+    //167-- Move every zero to the right side of a given array of integers
+    //168-- Multiply two specified integers without using the multiply operator
+    //169-- Reverse the content of a sentence without reverse every word
+    //170-- Find the length of the longest consecutive sequence of a given array of integers
+    //171-- Accept two string and test if the second string contains the first one
+    //172-- Get the number of element that are smaller than the number of another
+    //173-- Find the median of the number inside the window
+    //174-- Find the maximum number inside the number in the window
+    //175-- Delete a specified node in the middle of a singly linked list
+    //176-- Partition an given array of integers into even number first and odd number second
+    //177-- Get a new binary tree with same structure and same value of a given binary tree
+    //178-- Find the longest increasing continuous subsequence in a given array of integers
+    //179-- Plus one to the number of a specified positive numbers represented as an array of digits
+    //180-- Swap every two adjacent nodes of a given linked list
+    //181-- Find the length of last word of a specified string
+    //182-- Check if two binary trees are identical or not
+    //183-- Accept a positive number and repeatedly add all its digits until the result has only one digit
+    //184-- Find the length of the longest consecutive sequence path of a given binary tree
+    //185-- Check if two specified strings are isomorphic or not
+    //186-- Check if a number is a strobogrammatic number
+    //187-- Find the index of first non-repeating character in a given string
+    //188-- Find all the start indices of a given string's anagrams in another specified string
+    //189-- Given two non-negative integers represented as string, return their sum
+    //190-- Find the missing string from two specified strings
+    //191-- Test whether there are two integers x and y
+    //192-- Rearrange the alphabets in the order followed by the sum of digits
+    //193-- Sum of all the elements from all possible subsets of a set formed by first n natural numbers
+    //194-- Find the all positions of a given number in a given matrix
+    //195-- Check if three given side lengths can make a triangle or not
+    //196-- Create a spiral array of n * n sizes from a given integer n
+    //197-- Test if a given number is a perfect square or not
+    //198-- Get the position of a given prime number
+    //199-- Check a string follows a specified pattern
+    //200-- Remove duplicate letters and arrange in lexicographical order
+    //201-- Divide a given array of integers into given k non-empty subsets whose sums are all equal
+    //202-- Find the total number of continuous subarrays in a specified array of integers
+    //203-- Find the total number of continuous subarrays in a specified array of integers
+    //204-- Compute xn % y where x, y and n are all 32bit integers
+    //205-- Check whether an given integer is power of 2 or not using O(1) time
+    //206-- Generate a crc32 checksum of a given string or byte array
+    //207-- Merge two sorted (ascending) linked lists in ascending order
+    //208-- Create a basic string compression method using the counts of repeated characters
+    //209-- Find all unique combinations from a collection of candidate numbers
+    //210-- Match any single character (use ?) or any sequence of characters (use *) including the empty
+    //211-- Find heights of the top three building in descending order from eight given buildings
+    //212-- Compute the digit number of sum of two given integers
+    //213-- Check whether three given lengths of three sides form a right triangle
+    //214-- Print the values of x, y where a, b, c, d, e and f are specified
+    //215-- Compute the amount of the debt in n months
+    //216-- Reads an integer n and find the number of combinations of a,b,c and d will be equal to n
+    //217-- Print the number of prime numbers which are less than or equal to a given integer
+    //218-- Compute the radius and the central coordinate of a circle
+    //219-- Check if a point (x, y) is in a triangle or not
+    //220-- Compute and print sum of two given integers
+    //221-- Accepts six numbers as input and sorts them in descending order
+    //222-- Test whether two lines PQ and RS are parallel
+    //223-- Find the maximum sum of a contiguous subsequence from a given sequence of numbers
+    //224-- Test if circumference of two circles intersect or overlap
+    //225-- Reads a date and prints the day of the date
+    //226-- Print mode values from a given a sequence of integers
+    //227-- Reads a text and prints two words
+    //228-- Reads n digits (given) chosen from 0 to 9 and prints the number of combinations
+    //229-- Reads the two adjoined sides and the diagonal of a parallelogram and check whether the parallelogram is a rectangle or a rhombus
+    //230-- Replace a string "python" with "java" and "java" with "python" in a given string
+    //231-- Find the difference between the largest integer and the smallest integer which are created by 8 numbers from 0 to 9
+    //232-- Compute the sum of first n given prime numbers
+    //233-- Accept a even number from the user and create a combinations that express the given number as a sum of two prime numbers
+    //234-- Create maximum number of regions obtained by drawing n given straight lines
+    //235-- Test whether AB and CD are orthogonal or not
+    //236-- Sum of all numerical values embedded in a sentence
+    //237-- Read the mass data and find the number of islands
+    //238-- Restore the original string by entering the compressed string with this rule
+    //239-- Cut out words of 3 to 6 characters length from a given sentence not more than 1024 characters
+    //240-- Compute the maximum value of the sum of the passing integers
+    //241-- Find the number of combinations
+    //242-- Adds up columns and rows of given table as shown in the specified figure
+    //243-- Reads a list of pairs of a word and a page number, and prints the word and a list of the corresponding page numbers
+    //244-- Accepts a string from the user and check whether the string is correct or not
+    //245-- Accepts students name, id, and marks and display the highest score and the lowest score
+    //246-- Convert 3 digits positive number in given format
+    //247-- Accepts three integers and check whether sum of the first two given integers is greater than third one
+    //248-- Check if each letter of a given word is less than the one before it
+    //249-- Count the number of set bits in a 32-bit integer
     
 }

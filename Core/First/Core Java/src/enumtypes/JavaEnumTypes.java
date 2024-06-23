d61275d90fad81f19031cab34f43bc4d35c4ae35
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 16:38:43 +0600

    Add

---
+++ Core/First/Core Java/src/enumtypes/JavaEnumTypes.java
@@ -0,0 +1,13 @@
+
+package enumtypes;
+
+
+public class JavaEnumTypes {
+    
+    //1-- create an enum called "DaysOfWeek" representing the days of the week
+    //2-- create an enum called "Weekend" with constants representing the days of the weekend
+    //3-- implement an enum called "Shape" with constants representing different geometric shapes
+    //4-- implement an enum called "Direction" with constants representing the cardinal directions (North, South, East, West)
+    //5-- defines an enum called "Months" with constants representing the months of the year.
+    
+}
---
+++ Core/First/Core Java/src/inheritance/JavaInheritance.java
@@ -0,0 +1,17 @@
+
+package inheritance;
+
+public class JavaInheritance {
+    
+    //1-- Animal with a method called makeSound
+    //2-- Create a class called Vehicle with a method called drive(). Create a subclass called Car that overrides the drive() method to print "Repairing a car"
+    //3-- Create a class called Shape with a method called getArea and a subclass called Rectangle
+    //4-- Employee class with methods called work, getSalary
+    //5-- BankAccount class with methods called deposit() and withdraw()
+    //6-- Animal Class with a method move()
+    //7-- Person Class with methods called getFirstName() and getLastName()
+    //8-- Create a class called Shape with methods called getPerimeter() and getArea()
+    //9-- Vehicle class hierarchy
+    //10-- Employee Class Hierarchy
+    
+}

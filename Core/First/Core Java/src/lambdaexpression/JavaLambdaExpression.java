aa22ed61bd4301833412d0f9f2c7b61fe687c44d
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 16:59:35 +0600

    Add

--- Core/First/Core Java/src/lambdaexpression/JavaLambdaExpression.java
+++ Core/First/Core Java/src/lambdaexpression/JavaLambdaExpression.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lambdaexpression;
 
-/**
- *
- * @author Admin
- */
+
 public class JavaLambdaExpression {
+    //1-- Sum of two integers
+    //2-- Check if string is empty
+    //3-- Convert list of strings to uppercase and lowercase using Lambda expression in Java
+    //4-- Filter even and odd numbers from list using Lambda expression in Java
+    //5-- Sort strings in alphabetical order using Lambda expression in Java
+    //6-- Calculate average of doubles using Lambda expression in Java
+    //7-- Remove duplicates from list of integers using Lambda expression in Java
+    //8-- 
+    //9-- 
+    //10-- 
+    //11-- 
+    //12-- 
+    //13-- 
+    //14-- 
+    //15-- 
+    //16-- 
+    //17-- 
+    //18-- 
+    //19-- 
+    //20-- 
+    //21-- 
+    //22-- 
+    //23-- 
+    //24-- 
+    //25-- 
+    
     
 }

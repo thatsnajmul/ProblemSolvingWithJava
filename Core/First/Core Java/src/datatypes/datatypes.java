38dae4f9984206e1e204a12fd70565ee27d94e52
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 16:31:51 +0600

    Add

---
+++ Core/First/Core Java/src/datatypes/datatypes.java
@@ -0,0 +1,32 @@
+
+package datatypes;
+
+
+public class datatypes {
+    
+    //1-- Fahrenheit to Celsius degree
+    //2-- Inches to meters
+    //3-- Adds all the digits in the integer between 0 and 1000
+    //4-- Takes the user to input the minutes, and print the number of years and days for the minutes
+    //5-- Print the current time in GMT
+    //6-- Compute body mass index (BMI)
+    //7-- Calculate speed in meters per second, kilometers and miles per hour
+    //8-- Reads a number and display the square, cube, and fourth power
+    //9-- Print the sum, difference, product, average, distance, maximum and minimum
+    //10-- Breaks an integer into a sequence of individual digits
+    //11-- Test whether a given double/float value is a finite floating-point value or not
+    //12-- Compare two given signed and unsigned numbers
+    //13-- Compute the floor division and the floor modulus of the given dividend and divisor
+    //14-- Extract the primitive type value from a given BigInteger value
+    //15-- Get the next floating-point adjacent in the direction of positive and negative infinity from a given float/double number
+    
+    
+    
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            System.out.println("//"+i+"-- ");
+        }
+    }
+    
+    
+}

07e60382e7d20eff3f02b84e965363e90a89b07b
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 16:01:22 +0600

    Add

--- Core/First/BasicPart-I/src/javaenumtypes/JavaEnumTypes.java
+++ Core/First/BasicPart-I/src/javaenumtypes/JavaEnumTypes.java
@@ -10,4 +10,10 @@ package javaenumtypes;
  */
 public class JavaEnumTypes {
     
+    //1-- create an enum called "DaysOfWeek" representing the days of the week
+    //2-- create an enum called "Weekend" with constants representing the days of the weekend
+    //3-- implement an enum called "Shape" with constants representing different geometric shapes
+    //4--  implement an enum called "Direction" with constants representing the cardinal directions (North, South, East, West)
+    //5-- 
+    
 }

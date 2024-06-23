f84212955487339daa4893a7c888247c4a2aef51
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 16:11:52 +0600

    Add

--- Core/First/BasicPart-I/src/exceptionhandling/ExceptionHandling.java
+++ Core/First/BasicPart-I/src/exceptionhandling/ExceptionHandling.java
@@ -6,10 +6,10 @@ public class ExceptionHandling {
     
     //1-- Try-Catch block example
     //2-- Method to check and handle odd numbers
-    //3-- 
-    //4-- 
-    //5-- 
-    //6-- 
-    //7-- 
+    //3-- File reading method with exception handling
+    //4-- File reading and exception handling for positive numbers
+    //5-- File reading and empty file exception handling
+    //6-- User input validation for duplicate integers
+    //7-- String input validation for vowels
     
 }

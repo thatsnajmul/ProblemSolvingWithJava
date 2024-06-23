9899d072449626784f23d736b29a98c5719d5e4d
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 16:03:37 +0600

    Add

--- Core/First/BasicPart-I/src/array/Array.java
+++ Core/First/BasicPart-I/src/array/Array.java
@@ -4,4 +4,84 @@ package array;
 
 public class Array {
     
+    //1-- Sort a numeric array and a string array
+    //2-- Sum values of an array
+    //3-- Print the specified grid
+    //4-- Calculate the average value of array elements
+    //5-- Test if an array contains a specific value
+    //6-- Find the index of an array element
+    //7-- Remove a specific element from an array
+    //8-- Copy an array by iterating the array
+    //9-- Insert an element into an array
+    //10-- Find the maximum and minimum value of an array
+    //11-- Reverse an array of integer values
+    //12-- Find the duplicate values of an array of integer values
+    //13-- Find the duplicate values of an array of string values
+    //14-- Find the common elements between two arrays
+    //15-- Find the common elements between two arrays of integers
+    //16-- Remove duplicate elements from an array
+    //17-- Find the second largest element in an array
+    //18-- Find the second smallest element in an array
+    //19-- Add two matrices of the same size
+    //20-- Convert an array to ArrayList
+    //21-- Convert an ArrayList to an array
+    //22-- Find all Pairs of elements in an array whose sum is equal to a specified number
+    //23-- Test the equality of two arrays
+    //24-- Find a missing number in an array
+    //25-- Find common elements from three sorted arrays
+    //26-- Move all 0's to the end of an array
+    //27-- Find the number of even and odd integers in a given array of integers
+    //28-- Get the difference between the largest and smallest values in an array of integers
+    //29-- Compute the average value of an array of integers except the largest and smallest values
+    //30-- Check if an array of integers without two specified numbers
+    //31-- Check if the sum of all the 10's in the array is exactly 30
+    //32-- Check if an array of integers contains two specified elements
+    //33-- Remove the duplicate elements of a given array and return the new length of the array
+    //34-- Find the length of the longest consecutive elements sequence from a given unsorted array of integers
+    //35-- Find the sum of the two elements of a given array which is equal to a given integer
+    //36-- Find all the unique triplets such that sum of all the three elements equal to a specified number
+    //37-- Create an array of its anti-diagonals from a given square matrix
+    //38-- Get the majority element from a given array of integers containing duplicates
+    //39-- Print all the LEADERS in the array
+    //40-- Find the two elements from a given array of positive and negative numbers such that their sum is closest to zero
+    //41-- Find smallest and second smallest elements of a given array
+    //42-- Segregate all 0s on left side and all 1s on right side of a given array of 0s and 1s
+    //43-- Find all combination of four elements of a given array whose sum is equal to a given value
+    //44-- 
+    //45-- 
+    //46-- 
+    //47-- 
+    //48-- 
+    //49-- 
+    //50-- 
+    //51-- 
+    //52-- 
+    //53-- 
+    //54-- 
+    //55-- 
+    //56-- 
+    //57-- 
+    //58-- 
+    //59-- 
+    //60-- 
+    //61-- 
+    //62-- 
+    //63-- 
+    //64-- 
+    //65-- 
+    //66-- 
+    //67-- 
+    //68-- 
+    //69-- 
+    //70-- 
+    //71-- 
+    //72-- 
+    //73-- 
+    //74-- 
+    //75-- 
+    //76-- 
+    //77-- 
+    //78-- 
+    //79-- 
+    
 }

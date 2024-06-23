38ed2a20ff6318f7fc9e7d9c0b8396414068ab4a
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 17:06:04 +0600

    Add

--- Core/First/Core Java/src/lambdaexpression/JavaLambdaExpression.java
+++ Core/First/Core Java/src/lambdaexpression/JavaLambdaExpression.java
@@ -10,24 +10,24 @@ public class JavaLambdaExpression {
     //5-- Sort strings in alphabetical order using Lambda expression in Java
     //6-- Calculate average of doubles using Lambda expression in Java
     //7-- Remove duplicates from list of integers using Lambda expression in Java
-    //8-- 
-    //9-- 
-    //10-- 
-    //11-- 
-    //12-- 
-    //13-- 
-    //14-- 
-    //15-- 
-    //16-- 
-    //17-- 
-    //18-- 
-    //19-- 
-    //20-- 
-    //21-- 
-    //22-- 
-    //23-- 
-    //24-- 
-    //25-- 
+    //8-- Calculate factorial using Lambda expression in Java
+    //9-- Check prime number using Lambda expression in Java
+    //10-- Concatenate strings using Lambda expression in Java
+    //11-- Find maximum and minimum values with Lambda expression in Java
+    //12-- Multiply and sum elements with Lambda expression in Java
+    //13-- count words in a sentence using lambda expression
+    //14-- check Palindrome string using lambda expression
+    //15-- calculate sum of squares of odd and even numbers using lambda expression
+    //16-- check if a list contains a specific word using lambda expression
+    //17-- find length of longest and smallest string using lambda expression
+    //18-- check if a number is a perfect square using lambda expression
+    //19-- Find second largest and smallest element in an Array
+    //20-- Sort list of objects with lambda expression
+    //21-- Calculate sum of prime numbers with lambda expression
+    //22-- Lambda expression for checking uppercase, lowercase, or mixedcase strings
+    //23-- Lambda expression to find average string length
+    //24-- Lambda expression to find largest prime factor
+    //25-- Lambda expression to convert integer to binary
     
     
 }

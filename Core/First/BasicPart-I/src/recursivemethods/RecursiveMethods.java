f40ca5cb8188822860a2982d0a9e56f00b183f28
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 16:00:27 +0600

    Add

--- Core/First/BasicPart-I/src/recursivemethods/RecursiveMethods.java
+++ Core/First/BasicPart-I/src/recursivemethods/RecursiveMethods.java
@@ -9,5 +9,20 @@ package recursivemethods;
  * @author Admin
  */
 public class RecursiveMethods {
+        //1-- Calculate the factorial of a number
+        //2-- Calculate the sum of numbers from 1 to n
+        //3-- Calculate the nth Fibonacci number
+        //4-- String palindrome detection
+        //5-- Calculate Base to Power
+        //6-- Reverse a given string
+        //7-- Find the greatest common divisor
+        //8-- Count occurrences of a specific element
+        //9-- Sum of odd numbers in an array
+        //10-- Find the length of a string
+        //11-- 
+        //12-- 
+        //13-- 
+        //14-- 
+        //15-- 
     
 }

3644f972f53c760ad309f8bf9c7e18bbf0da1b35
thatsnajmul <thatsnajmul@yahoo.com>
2024-06-23 17:13:41 +0600

    Add

--- Core/First/Core Java/src/genericmethod/JavaGenericMethod.java
+++ Core/First/Core Java/src/genericmethod/JavaGenericMethod.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package genericmethod;
 
-/**
- *
- * @author Admin
- */
 public class JavaGenericMethod {
-    
+    //1-- Check equality of arrays
+    //2-- Calculate sum of even and odd numbers
+    //3-- Find index of target element in list
+    //4-- Reverse list elements
+    //5-- Merge two lists alternately
+    //6-- Filter list with predicate
+    //7-- Print key-value pairs in a map
 }
